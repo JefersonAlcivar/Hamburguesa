23eb464879bd64b66921f050e50ad94b3bdf0da0
Jeferson Albero Alcivar Briones <albertoalcivar15@gmail.com>
2021-11-19 19:17:59 -0500

    Agregar archivos de proyecto.

---
+++ Hamburguesa/Menu_Ventas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NEGOCIO_DE_HAMBURGUESAS
+{
+    class Menu_Ventas
+    {
+        //ingresar nuevos pedidos
+        public List<Pedidos> IngresarPedidos(List<Pedidos> ListaPedidos, List<Cliente> ListaCliente, List<Factura> ListaFactura)
+        {
+            return (ListaPedidos);
+        }
+        //ingresar nuevos clientes
+        public List<Cliente> IngresarCliente(List<Pedidos> ListaPedidos, List<Cliente> ListaCliente, List<Factura> ListaFactura)
+        {
+            return (ListaCliente);
+        }
+        //facturas
+        public List<Factura> IngresarFactura(List<Pedidos> ListaPedidos, List<Cliente> ListaCliente, List<Factura> ListaFactura)
+        {
+            return (ListaFactura);
+        }
+
+    }
+}
---
+++ Hamburguesa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Hamburguesa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ELEGIR LISTA
+            String elegir;
+            List<Pedidos> ListaPedidos = new List<Pedidos>();
+            List<Cliente> ListaCliente = new List<Cliente>();
+            List<Factura> ListaFactura = new List<Factura>();
+        }
+    }
+}
+
