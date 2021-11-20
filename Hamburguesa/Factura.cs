37ee54631b5704f382030ee75744880db86b82a0
JOAN JOB GALLARDO ALCIVAR <e1313197400@live.uleam.edu.ec>
2021-11-19 19:41:38 -0500

    Creación de la clase factura, se realizaron propiedades donde se almacenara los datos del cliente.

---
+++ Hamburguesa/Factura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hamburguesa
+{
+    public String Cedula { get; set; }
+    public String Nombre { get; set; }
+    public String Hamburguesa { get; set; }
+    public String Tamaño { get; set; }
+    public int Precio { get; set; }
+    //Mostrar las listas de facturas y pedidos
+    public List<Factura> MostrarFacturas(List<Pedidos> ListaPedidos, List<Factura> ListaFacturas)
+    {
+        return ListaFacturas;
+    }
+    //facturas menores a diez
+    public void MostrarFacturasMenores(List<Factura> ListaFacturas, List<Pedidos> ListaPedidos)
+    {
+
+    }
+}
+}
