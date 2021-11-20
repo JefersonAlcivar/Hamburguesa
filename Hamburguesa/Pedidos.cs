9f2296add80600e581b30f4ac76a18f7125e089c
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2021-11-19 21:46:20 -0500

    Pedidos
    pedidos actualizados por luis tuarez

--- Hamburguesa/Pedidos.cs
+++ Hamburguesa/Pedidos.cs
@@ -1,10 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Text;
-
 namespace Hamburguesa
 {
-    class Pedidos
+    class Pedidos : Menu_Ventas
     {
+        public String Cedula { get; set; }
+        public String Nombre { get; set; }
+        public String Hamburguesa { get; set; }
+        public String Tamaño { get; set; }
+        public int Precio { get; set; }
+
+        public List<Pedidos> EliminarPedido(List<Pedidos> ListaPedido, string cedula)
+        {
+            return ListaPedido;
+        }
+        //EN ESTE APARTADO PODEMOS MODIFICAR LOS PEDIDOS
+        public List<Pedidos> ModificarPedido(List<Pedidos> ListaPedido, string cedula, string nombre)
+        {
+            return ListaPedido;
+        }
+        //METODO CREAR LOS PEDIDOS
+        public Pedidos CrearPedidos(List<Pedidos> ListaPedidos, string Cedula, string Nombre)
+        {
+            
+        }
+        // mostrar solo  las precios mayores o iguales a diez de los pedidos
+        public void MostrarPedidosMayores(List<Pedidos> ListaPedidos)
+        {
+
+        }
+
+        public int validar(List<Pedidos> ListaPedidos, string Cedula)
+        {
+
+        }
     }
+
 }
