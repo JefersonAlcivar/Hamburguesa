0d2ba8389ca94b9c3f20c4d5da9406cdca10791b
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2022-01-06 12:37:49 -0500

    Update Factura.cs

--- Hamburguesa/Factura.cs
+++ Hamburguesa/Factura.cs
@@ -1,23 +1,27 @@
-﻿using System;
+//debido a un error del git, nos vimos la necesidad de subir el codigo manuelamente 
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
-namespace Hamburguesa
+namespace NEGOCIO_DE_HAMBURGUESAS
 {
-    public String Cedula { get; set; }
-    public String Nombre { get; set; }
-    public String Hamburguesa { get; set; }
-    public String Tamaño { get; set; }
-    public int Precio { get; set; }
-    //Mostrar las listas de facturas y pedidos
-    public List<Factura> MostrarFacturas(List<Pedidos> ListaPedidos, List<Factura> ListaFacturas)
+    class Factura
     {
-        return ListaFacturas;
-    }
-    //facturas menores a diez
-    public void MostrarFacturasMenores(List<Factura> ListaFacturas, List<Pedidos> ListaPedidos)
-    {
+        public String Cedula { get; set; }
+        public String Nombre { get; set; }
+        public String Hamburguesa { get; set; }
+        public String Tamaño { get; set; }
+        public int Precio { get; set; }
+        //Mostrar las listas de facturas y pedidos
+        public List<Factura> MostrarFacturas(List<Pedidos> ListaPedidos, List<Factura> ListaFacturas)
+        {
+            
+            return ListaFacturas;
+        }
+        //facturas menores a diez
+        public void MostrarFacturasMenores(List<Factura> ListaFacturas, List<Pedidos> ListaPedidos)
+        {
 
+        }
     }
-}
 }
