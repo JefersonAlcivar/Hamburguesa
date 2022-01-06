f86f58e288d28ceb45c10a2d8ebaacb110e9b2ec
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2022-01-06 12:39:17 -0500

    Update Program.cs

--- Hamburguesa/Program.cs
+++ Hamburguesa/Program.cs
@@ -1,16 +1,47 @@
-﻿using System;
+// debido a un error del git, nos vimos a la necesiada de colocar el codigo manualmente
+using System;
 using System.Collections.Generic;
-namespace Hamburguesa
+namespace NEGOCIO_DE_HAMBURGUESAS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //ELEGIR LISTA
             String elegir;
             List<Pedidos> ListaPedidos = new List<Pedidos>();
             List<Cliente> ListaCliente = new List<Cliente>();
             List<Factura> ListaFactura = new List<Factura>();
+            Console.Clear();
+            Console.WriteLine("*NEGOCIO DE HAMBURGUESAS*" + "\n 1.Pedidos" + "\n 2.Cliente" + "\n 3.Factura" + "\n 4.Salir");
+            elegir = Console.ReadLine();
+            Menu_Ventas Elegir = new Menu_Ventas();
+            while (!elegir.Equals("4"))
+            {
+                switch (elegir)
+                {
+                    case "1":
+                        Elegir.IngresarPedidos(ListaPedidos, ListaCliente, ListaFactura);
+                        break;
+                    case "2":
+                        Elegir.IngresarCliente(ListaPedidos, ListaCliente, ListaFactura);
+                        break;
+                    case "3":
+                        Elegir.IngresarFactura(ListaPedidos, ListaCliente, ListaFactura);
+                        break;
+                }
+                if (!elegir.Equals("4"))
+                {
+                    Console.Clear();
+                    Console.WriteLine("-----Menu Ventas PASTELERIA FACCI-----" + "\n 1.Pedidos" + "\n 2.Cliente" + "\n 3.Factura" + "\n 4.Salir");
+                    elegir = Console.ReadLine();
+                }
+                else
+                {
+                    elegir = "4";
+                }
+            }
+
+
         }
     }
 }
