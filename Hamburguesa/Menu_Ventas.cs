cafd4aa1e1258ea255d8aae5f4eaf3e1f18a2509
karentumbaco <94723538+karentumbaco@users.noreply.github.com>
2022-01-06 12:36:06 -0500

    Update Menu_Ventas.cs

--- Hamburguesa/Menu_Ventas.cs
+++ Hamburguesa/Menu_Ventas.cs
@@ -1,5 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+
+// Con la ayuda del segundo principio del metodo SOLID (Open/Close) permite observar que al momento de querer modificar la clase no afecta
+// a las otras clases que se encuentran rotundamente cerrados para una modificacion pero aun pueden crearse clases
+// como extension y para despues agregarse en esta clase madre para ingresar y hacer todos los debidos adaptamientos
+// al programa.
 namespace NEGOCIO_DE_HAMBURGUESAS
 {
     class Menu_Ventas
@@ -7,18 +12,128 @@ namespace NEGOCIO_DE_HAMBURGUESAS
         //ingresar nuevos pedidos
         public List<Pedidos> IngresarPedidos(List<Pedidos> ListaPedidos, List<Cliente> ListaCliente, List<Factura> ListaFactura)
         {
+
             return (ListaPedidos);
         }
         //ingresar nuevos clientes
         public List<Cliente> IngresarCliente(List<Pedidos> ListaPedidos, List<Cliente> ListaCliente, List<Factura> ListaFactura)
         {
+            Cliente verificacion = new Cliente();
+            Boolean oc = true;
+            while (oc)
+            {
+                Console.Clear();
+                int c = 0;
+                String elegir = "1";
+                Boolean opp = true;
+                Console.WriteLine("    Clientes    ");
+                Console.WriteLine("Cedula\tNombre");
+                foreach (var item in ListaCliente)
+                {
+                    Console.WriteLine(item.Cedula + "\t" + item.Nombre);
+                    c = c + 1;
+                }
+
+                if (c == 0)
+                {
+                    Console.WriteLine("No hay clientes en el sistema, por favor ingrese un cliente para poder continuar");
+                    while (opp)
+                    {
+                        Console.WriteLine(" 1.Ingresar Clientes" + "\n 2. Volver");
+                        elegir = Console.ReadLine();
+                        switch (elegir)
+                        {
+                            case "1":
+                                Cliente nuevocliente = new Cliente();
+                                nuevocliente = nuevocliente.CrearCliente(ListaCliente);
+                                ListaCliente.Add(nuevocliente);
+                                opp = false;
+                                break;
+                            case "2":
+                                opp = false; oc = false;
+                                break;
+                            default:
+                                Console.WriteLine("No existe esta opcion, por favor ingrese una valida");
+                                opp = true;
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    while (opp)
+                    {
+                        Console.WriteLine(" 1.Ingresar Clientes" + "\n 2. Modificar clientes" + "\n 3. Eliminar Cliente" + "\n 4. Mostrar Clientes" + "\n 5. Volver");
+                        elegir = Console.ReadLine();
+                        switch (elegir)
+                        {
+                            case "1":
+                                Cliente nuevocliente = new Cliente();
+                                nuevocliente = nuevocliente.CrearCliente(ListaCliente);
+                                ListaCliente.Add(nuevocliente);
+                                opp = false;
+                                break;
+                            case "2":
+                                Cliente modcliente = new Cliente();
+                                modcliente.ModificarCliente(ListaCliente);
+                                opp = false;
+                                break;
+                            case "3":
+                                Cliente elicliente = new Cliente();
+                                elicliente.EliminarCliente(ListaCliente);
+                                opp = false;
+                                break;
+                            case "4":
+                                Cliente moscliente = new Cliente();
+                                moscliente.MostrarClientes(ListaCliente);
+                                opp = false;
+                                break;
+                            case "5":
+                                opp = false; oc = false;
+                                break;
+                            default:
+                                Console.WriteLine("No existe esta opcion, por favor ingrese una valida");
+                                opp = true;
+                                break;
+                        }
+                    }
+                }
+            }
             return (ListaCliente);
+
         }
         //facturas
         public List<Factura> IngresarFactura(List<Pedidos> ListaPedidos, List<Cliente> ListaCliente, List<Factura> ListaFactura)
         {
+            Console.Clear();
+            string elegir;
+            Boolean oc = true;
+            while (oc)
+            {
+                Console.WriteLine(" 1.Imprimir Facturero" + "\n 2. mostrar facturas menores a 10$" + "\n 3. Volver");
+                elegir = Console.ReadLine();
+                switch (elegir)
+                {
+                    case "1":
+                        Factura facturero = new Factura();
+                        facturero.MostrarFacturas(ListaPedidos, ListaFactura);
+                        oc = false;
+                        break;
+                    case "2":
+                        Factura mosFac = new Factura();
+                        mosFac.MostrarFacturasMenores(ListaFactura, ListaPedidos);
+                        oc = false;
+                        break;
+                    case "3":
+                        oc = false;
+                        break;
+                    default:
+                        Console.WriteLine("No existe esta opcion, por favor ingrese una valida");
+                        oc = true;
+                        break;
+                }
+            }
             return (ListaFactura);
         }
-
     }
 }
