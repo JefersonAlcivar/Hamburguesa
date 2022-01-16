b539b7bb3e81ca9174f002c569ca79330d89b64b
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2022-01-15 20:17:16 -0500

    Update Pedidos.cs

--- Hamburguesa/Pedidos.cs
+++ Hamburguesa/Pedidos.cs
@@ -1,5 +1,7 @@
+//Se subio la case por completo devido a un error de git, sin embargo la clase ya esta 100% completa
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NEGOCIO_DE_HAMBURGUESAS
 {  //El patrón utilizado que es Single responsability la cual indica que cada clase solo tiene una razón de ser 
@@ -98,6 +100,7 @@ namespace NEGOCIO_DE_HAMBURGUESAS
         //METODO CREAR LOS PEDIDOS
         public Pedidos CrearPedidos(List<Pedidos> ListaPedidos, string Cedula, string Nombre)
         {
+            int precio = 0;
             string ingreso = "";
             bool op = true;
             Pedidos nuevopedi = new Pedidos();
@@ -111,21 +114,25 @@ namespace NEGOCIO_DE_HAMBURGUESAS
                 {
                     case "1":
                         ingreso = "Especial";
+                        precio = precio + 5;
                         op = false;
                         break;
 
                     case "2":
                         ingreso = "Sencilla";
+                        precio = precio + 3;
                         op = false;
                         break;
 
                     case "3":
                         ingreso = "Completa";
+                        precio = precio + 6;
                         op = false;
                         break;
 
                     case "4":
                         ingreso = "Mega Hamburgesa";
+                        precio = precio + 8;
                         op = false;
                         break;
 
@@ -135,16 +142,59 @@ namespace NEGOCIO_DE_HAMBURGUESAS
                         break;
                         
                 }
-            
             }
-            return nuevopedi;
-        }
+            //aqui
+            nuevopedi.Hamburguesa = ingreso;
+            op = true;
+            while (op)
+            {
+                Console.WriteLine("Seleccione sabor de la bebida \n 1. Coca-Cola  \n 2. Pepsi  \n 3. Gallito");
+                ingreso = Console.ReadLine();
+                switch (ingreso)
+                {
+                    case "1":
+                        ingreso = "Coco-cola";
+                        precio=precio+2;
+                        op = false;
+                        break;
+
+                    case "2":
+                        ingreso = "Pepsi";
+                        precio=precio+2;
+                        op = false;
+                        break;
+
+                    case "3":
+                        ingreso = "Gallito";
+                        precio=precio+2;
+                        op = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("No es un sabor valido por favor seleccione uno correcto");
+                        op = true;
+                        break;
+                }
+            }
+            nuevopedi.Tamaño = ingreso;
+            nuevopedi.Precio = precio;
+            return (nuevopedi);
+        }//Aqui
         // mostrar solo  las precios mayores o iguales a diez de los pedidos
         public void MostrarPedidosMayores(List<Pedidos> ListaPedidos)
         {
-        
-        }
-
+            
+                //lambda utilizando  where para mostrar solo  las valores mayores o iguales a 10 de los pedidos
+                IEnumerable<Pedidos> Consulta = ListaPedidos.Where(x => x.Precio >= 10).Select(x => x);
+            //se lee lo almacenado en la lista consulta
+                foreach (var item in Consulta)
+                {
+                //se imprime la cédula, nombre, y precio
+                    Console.WriteLine(item.Cedula + "\t" + item.Nombre + "\t" + item.Precio);
+                }
+            Console.ReadKey();
+            }
+        //ghgk
         public int validar(List<Pedidos> ListaPedidos, string Cedula)
         {
             int x = 1, f = 0;
