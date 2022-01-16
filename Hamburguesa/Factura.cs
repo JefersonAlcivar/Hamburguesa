a3afd15b0cb5099fa80a49cd37f7316aea4a8a5f
karentumbaco <94723538+karentumbaco@users.noreply.github.com>
2022-01-15 20:22:35 -0500

    Update Factura.cs

--- Hamburguesa/Factura.cs
+++ Hamburguesa/Factura.cs
@@ -1,4 +1,3 @@
-//debido a un error del git, nos vimos la necesidad de subir el codigo manuelamente 
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,13 +14,39 @@ namespace NEGOCIO_DE_HAMBURGUESAS
         //Mostrar las listas de facturas y pedidos
         public List<Factura> MostrarFacturas(List<Pedidos> ListaPedidos, List<Factura> ListaFacturas)
         {
-            
+            ListaFacturas.Clear();
+            Factura facturero = new Factura();
+            //Lamba con select para hacer el cambio
+            IEnumerable<Pedidos> Listacambio = ListaPedidos.Select(x => x);
+            foreach (var item in Listacambio)
+            {
+
+                facturero.Cedula = item.Cedula;
+                facturero.Nombre = item.Nombre;
+                facturero.Hamburguesa = item.Hamburguesa;
+                facturero.Tamaño = item.Tamaño;
+                facturero.Precio = item.Precio;
+                ListaFacturas.Add(facturero);
+            }
+
+            //Lamba con select para mostrar el facturero
+            IEnumerable<Pedidos> Listanombre = ListaPedidos.Select(x => x);
+            foreach (var item in Listanombre)
+            {
+                Console.WriteLine(item.Cedula + "\t" + item.Nombre + "\t" + item.Hamburguesa + "\t" + item.Tamaño + "\t" + item.Precio);
+            }
+            Console.ReadKey();
             return ListaFacturas;
         }
-        //facturas menores a diez
         public void MostrarFacturasMenores(List<Factura> ListaFacturas, List<Pedidos> ListaPedidos)
         {
-
+            //lamba con where para mostrar solo  las precios mayores o iguales a 10 de los pedidos
+            IEnumerable<Pedidos> Consulta = ListaPedidos.Where(x => x.Precio < 10).Select(x => x);
+            foreach (var item in Consulta)
+            {
+                Console.WriteLine(item.Cedula + "\t" + item.Nombre + "\t" + item.Precio);
+            }
+            Console.ReadKey();
         }
     }
 }
